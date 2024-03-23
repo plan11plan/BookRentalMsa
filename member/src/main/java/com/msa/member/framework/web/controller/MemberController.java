a50b57d2d77e94865b1ebb7064450127a689863c
plan11plan <tjtfy7028@gmail.com>
2024-03-23 17:53:25 +0900

    [feat] memberMsa

---
+++ member/src/main/java/com/msa/member/framework/web/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.msa.member.framework.web.controller;
+
+
+import com.msa.member.application.usecase.AddMemberUsecase;
+import com.msa.member.application.usecase.InquiryMemberUsecase;
+import com.msa.member.framework.web.dto.MemberInfoDTO;
+import com.msa.member.framework.web.dto.MemberOutputDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class MemberController {
+    private final AddMemberUsecase addMemberUsecase;
+    private final InquiryMemberUsecase inquiryMemberUsecase;
+
+    @PostMapping("/Member/")
+    public ResponseEntity<MemberOutputDTO> addMember(@RequestBody MemberInfoDTO memberInfoDTO) {
+        MemberOutputDTO addedMember =
+                addMemberUsecase.addMember(memberInfoDTO);
+        return new ResponseEntity<>(addedMember, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/Member/{no}")
+    public ResponseEntity<MemberOutputDTO> getMember(@PathVariable("no") long no) {
+        MemberOutputDTO member = inquiryMemberUsecase.getMember(no);
+        return member != null
+                ? new ResponseEntity<>(member, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
