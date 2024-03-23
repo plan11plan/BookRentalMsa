84c4e63f9b6838e8bc152f7ad1b53579c291998c
plan11plan <tjtfy7028@gmail.com>
2024-03-23 18:07:05 +0900

    [feat] bestBook firstFormat

---
+++ bestBook/src/main/java/com/msa/bestBook/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+//package com.msa.rental.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration
+//public class SwaggerConfig {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+////                .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드를 표시할 것이면 true
+////                .select()
+////                .apis(RequestHandlerSelectors.basePackage("com.rental.rentalMs"))
+////                .paths(PathSelectors.any())
+////                .build();
+//                .ignoredParameterTypes(java.sql.Date.class)
+//                .forCodeGeneration(true)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build()
+//                .enable(true);
+//    }
+//}
